240d00b7fa3bc57ceeda02047670983105845a40
Cordero Core <127983572+uwcdc@users.noreply.github.com>
2024-04-11 00:52:13 +0000

    refactor: Update Dockerfile to use pangeo/base-image:master

---
+++ data-science-nlp/postBuild.sh
@@ -0,0 +1,2 @@
+# Install the latest version of PyTorch nightly build (with GPU support)
+python3 -m pip3 install --pre torch --force-reinstall --index-url https://download.pytorch.org/whl/nightly/cu117
