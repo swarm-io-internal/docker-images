a2895020b978e3a0d7c1c155c03b69332fe29970
Cordero Core <127983572+uwcdc@users.noreply.github.com>
2024-04-11 01:59:22 +0000

    fix: Remove postBuild and add torch install to environment.yml

--- data-science-nlp/postBuild.sh
+++
@@ -1,2 +0,0 @@
-# Install the latest version of PyTorch nightly build (with GPU support)
-python3 -m pip3 install --pre torch --force-reinstall --index-url https://download.pytorch.org/whl/nightly/cu117
